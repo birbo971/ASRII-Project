416317bd20fd65039ecfb5465201f0b122f30e44
CorentinPuaud <37401262+CorentinPuaud@users.noreply.github.com>
2018-05-28 10:05:56 +0200

    Bootstrap

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// check['age'] = function() {
+//
+//     var age = document.getElementById('age'),
+//         tooltipStyle = getTooltip(age).style,
+//         ageValue = parseInt(age.value);
+//
+//     if (!isNaN(ageValue) && ageValue >= 5 && ageValue <= 140) {
+//         age.className = 'correct';
+//         tooltipStyle.display = 'none';
+//         return true;
+//     } else {
+//         age.className = 'incorrect';
+//         tooltipStyle.display = 'inline-block';
+//         return false;
+//     }
+//
+// };
+
+function enregistrer() {
+  alert("coucou");
+  console.log("coucou");
+}
