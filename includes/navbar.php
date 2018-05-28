06894251735e714885870d01a9f0218f8cef6de1
CorentinPuaud <37401262+CorentinPuaud@users.noreply.github.com>
2018-05-28 10:08:45 +0200

    Navbar

---
+++ includes/navbar.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-md bg-light navbar-light">
+  <img src="img/logo.png" alt="logo" id="logo">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="index.php">Inscription</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="liste.php">Liste d'inscription</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Liste de présence</a>
+      </li>
+    </ul>
+  </div>
+</nav>
