c075c5d0f048f057a91e202d70bc0978b56672d3
CorentinPuaud <37401262+CorentinPuaud@users.noreply.github.com>
2018-05-30 11:52:23 +0200

    Ajout des  programmes

---
+++ parcoursASR2I.php
@@ -0,0 +1,95 @@
+<?php
+$titre ='Objectifs et Rythmes';
+
+?>
+<?php include('includes/header.php'); ?>
+<?php include('includes/navbar.php'); ?>
+<div class="container">
+  <h1>Tronc commun</h1>
+  <h2>UE 4 - ADMINISTRATION ET SECURISATION DES RESEAUX ET SERVICES INTRANET ET INTERNET (196 H)</h2>
+  <h3>Interconnexion de réseaux, routage</h3>
+  <h4>Objectifs :</h4>
+  <ul>
+    <li>Comprendre les principes et les problèmes de sécurité dans la mise en oeuvre d’interconnexion de réseaux locaux</li>
+  </ul>
+  <h4>Programme :</h4>
+  <ul>
+    <li>Les protocoles ( RIP, IGRP, OSPF, BGP).</li>
+    <li>Les équipements Ponts, Commutateurs, routeurs.</li>
+    <li>Analyse de Trames avec EtherReal</li>
+  </ul>
+  <h3>Administration des Réseaux Windows</h3>
+  <h4>Objectifs :</h4>
+  <ul>
+    <li>Acquérir les compétences de base sur le système d’exploitation Windows 2003 Serveur</li>
+  </ul>
+  <h4>Programme :</h4>
+  <ul>
+    <li>Les cours sont essentiellement organisés en TP.</li>
+    <li>Présentation générale, principe des SE, des différents types de serveurs. Installation de Wins 2003 Seveur avec la mise en oeuvre des outils de diagnostiques Msconfig, RegEdit et de l’outil de déploiement Sysprep</li>
+    <li>Configuration des serveurs DNS, DHCP ainsi que des relais DHCP.</li>
+    <li>Installation de Active Directory, principe de GL / GDL/ GG / GU</li>
+    <li>Ecriture de Scripts JS pour la gestion des ressources LPAP commandes Dsadd…</li>
+    <li>Gestion des stratégies de groupes, création de nouvelles policys</li>
+    <li>Mise en oeuvre d’une forêt et de Sites Windows.</li>
+    <li>Mise en oeuvre du serveur IIS pour la gestion des Protocoles http, SMTP, FTP</li>
+    <li>Déploiement d’une PKI par clé publique et privée avec un serveur de certificats</li>
+    <li>Sécurisation des transactions HTTPS et par signatures numériques par certificats</li>
+    <li>Mise en oeuvre d’Exchange Serveur 2003.</li>
+  </ul>
+  <h3>Administration des Réseaux LINUX</h3>
+  <h4>Objectifs :</h4>
+  <ul>
+    <li>Acquérir les compétences de base sur l’administration système d’exploitation Linux.</li>
+  </ul>
+  <h4>Programme :</h4>
+  <ul>
+    <li>La gestion des utilisateurs et de l’espace disque</li>
+    <li>Sauvegarde des données</li>
+    <li>Linux en réseau (TCP/IP, TELNET, FTP, NFS)</li>
+    <li>La sécurité des applications</li>
+    <li>Mise en oeuvre d’un serveur WEB Apache avec sécurisation des accès.</li>
+    <li>Maîtriser les techniques de protection d’un serveur Web et de sa base de données</li>
+    <li>Mise en oeuvre d’un serveur SAMBA.</li>
+    <li>Mise en place des serveurs DNS primaire et secondaire</li>
+    <li>Acquérir les compétences de base sur les principes de création des virus / et antivirus</li>
+    <li>Comprendre la mise en oeuvre des protections courantes.</li>
+  </ul>
+  <h3>Sécurité Réseaux et des Applications</h3>
+  <h4>Objectifs :</h4>
+  <ul>
+    <li>Acquérir les compétences nécessaires à la configuration et la mise en place d’un outil de sécurité des réseaux et des applications web.</li>
+  </ul>
+  <h4>Programme :</h4>
+  <ul>
+    <li>Architecture & système de sécurité (Bastion…DMZ)</li>
+    <li>VPN,</li>
+    <li>FireWall, NAT,PAT IPtable, Netfilter</li>
+    <li>Proxies</li>
+    <li>IPSec</li>
+    <li>SSH, SSL,</li>
+    <li>DNSSEC</li>
+  </ul>
+  <h3>Programmation systèmes</h3>
+  <h4>Objectifs :</h4>
+  <ul>
+    <li>Il permettra aux étudiants de comprendre le rôle essentiel joué par le système d’exploitation à partir de l’exemple du système UNIX et de se familiariser avec les principales fonctions systèmes du noyau d’Unix permettant la communication et la synchronisation inter processus.</li>
+  </ul>
+  <h4>Programme :</h4>
+  <ul>
+    <li>Programmation de commandes.</li>
+    <li>Communication et synchronisation inter processus.</li>
+  </ul>
+  <h3>Ecriture approfondie de scripts Linux et Windows</h3>
+  <h4>Objectifs :</h4>
+  <ul>
+    <li>Acquérir les Connaissances techniques de l’écriture avancée de Scripts Linux et Windows 2003</li>
+  </ul>
+  <h4>Programme :</h4>
+  <ul>
+    <li>Mise en oeuvre d’interfaces pour l’automatisation de tâches systèmes avancées Linux et Windows Apprentissage des commandes à partir d’une console</li>
+  </ul>
+
+</div>
+
+<?php include('includes/footer.php') ?>
---
+++ troncCommun.php
@@ -0,0 +1,185 @@
+<?php
+$titre ='Objectifs et Rythmes';
+
+?>
+<?php include('includes/header.php'); ?>
+<?php include('includes/navbar.php'); ?>
+<div class="container">
+  <h1>Tronc commun</h1>
+
+  <h2>UE 1 – COMMUNICATION ET CONNAISSANCE DE L’ENTREPRISE (112 H)</h2>
+  <h3>Technique de Recherche d’emploi et Communication</h3>
+  <h4>Objectifs :</h4>
+  <ul>
+    <li>Rédiger un rapport.</li>
+    <li>Faire un compte-rendu.</li>
+    <li>Intervenir en réunion et présenter un projet.</li>
+  </ul>
+  <h4>Programme :</h4>
+  <ul>
+    <li>Rédaction de rapport</li>
+    <li>Rédaction de compte-rendu</li>
+    <li>Intervenir à l’oral en réunion.</li>
+    <li>Présenter à l’oral un projet.</li>
+  </ul>
+  <h3>Organisation et gestion de l’entreprise</h3>
+  <h4>Objectifs :</h4>
+  <ul>
+    <li>Comprendre les enjeux de l’entreprise.</li>
+  </ul>
+  <h4>Programme :</h4>
+  <ul>
+    <li>Fonctions des différentes structures juridiques.</li>
+    <li>Organisation générale de l’entreprise, culture économique de base.</li>
+    <li>Droits et Devoirs de l’employé</li>
+  </ul>
+  <h3>Gestion et management de projets</h3>
+  <h4>Objectifs :</h4>
+  <ul>
+    <li>Concevoir, organiser et gérer un projet informatique.</li>
+  </ul>
+  <h4>Programme :</h4>
+  <ul>
+    <li>Fonctions du cycle de vie d’un produit.</li>
+    <li>Caractéristiques des différentes étapes du développement.</li>
+    <li>Organisation et gestion du temps</li>
+  </ul>
+  <h3>Gestion de la qualité dans l’entreprise</h3>
+  <h4>Objectifs :</h4>
+  <ul>
+    <li>Comprendre les enjeux des normes qualités dans l’entreprise.</li>
+  </ul>
+  <h4>Programme :</h4>
+  <ul>
+    <li>La normalisation ISO</li>
+    <li>La notion de qualité</li>
+    <li>La notion de procédure</li>
+    <li>La décomposition de processus</li>
+    <li>Les plans d’action.</li>
+    <li>Les indicateurs et tableaux de bord associés</li>
+  </ul>
+  <h3>Anglais</h3>
+  <h4>Objectifs :</h4>
+  <ul>
+    <li>Permettre à l’apprenti une meilleure communication orale et écrite tant au niveau technique qu’au niveau courant.</li>
+  </ul>
+  <h4>Programme :</h4>
+  <ul>
+    <li>Diagnostic individuel de connaissances.</li>
+    <li>Exposé et analyse de documents techniques ou connexes</li>
+  </ul>
+
+  <h2>UE 2 – INTEGRATION DES RESEAUX INTRANET – INTERNET (116 H)</h2>
+  <h3>Architecture des Réseaux Locaux</h3>
+  <h4>Objectifs :</h4>
+  <ul>
+    <li>Acquérir les Connaissances de base des principaux composants réseaux.</li>
+  </ul>
+  <h4>Programme :</h4>
+  <ul>
+    <li>Les connaissances de base des principaux composants réseaux sous forme de cours magistraux.</li>
+    <li>Après une introduction aux concepts généraux du fonctionnement des réseaux, de leurs architectures en couches et des mécanismes de la transmission du signal, les protocoles du niveau physique des réseaux sont abordés en terme de fonctionnement, puis sont décrits les éléments caractéristiques d’un cabling system et les capacités des différents composants actifs ou passifs entrant dans les infrastructures physiques des réseaux locaux et réseaux d’opérateurs. Enfin pour offrir une vision d’ensemble un module complet présente une synthèse technologique des moyens actuels permettant la transmission des données (au sens générique du terme).</li>
+    <li>Les deux sous-modules suivants sont dédiés à la couche Liaison en focalisant particulièrement en ce qui concerne la couche MAC, sur le monde Ethernet et les hauts débits, puis en détaillant le fonctionnement de la couche LLC.</li>
+    <li>Les quatre sous-modules restants sont globalement dédiés à TCP/IP et aux environnements protocolaires courants qui sont associés à ce couple de protocoles.</li>
+  </ul>
+  <h3>Initiation Linux</h3>
+  <h4>Objectifs :</h4>
+  <ul>
+    <li>Acquérir les Connaissances des principales commandes systèmes Linux pour l’écriture de Scripts</li>
+    <li>Apprentissage des commandes à partir d’une console</li>
+    <li>Le système de fichiers</li>
+    <li>Les droits d’accès</li>
+    <li>Redirection d’entrée/Sorties</li>
+    <li>Programme utilitaires et filtres</li>
+    <li>La gestion des processus</li>
+  </ul>
+  <h3>Outils de configuration, Scripts Shell</h3>
+  <h4>Objectifs :</h4>
+  <ul>
+    <li>Ecrire un script Shell pour la configuration automatique d’une application</li>
+    <li>Scripts de sauvegarde</li>
+    <li>Gestion des utilisateurs et des ressources</li>
+  </ul>
+  <h3>Base de Données Relationnelle</h3>
+  <h4>Objectifs :</h4>
+  <ul>
+    <li>Acquérir les compétences de base sur les principes de création d’une Base de Données relationnelle</li>
+  </ul>
+  <h4>Programme :</h4>
+  <ul>
+    <li>Algèbre relationnelle</li>
+    <li>SQL2</li>
+    <li>MySQL</li>
+  </ul>
+  <h3>Langage Objet Java</h3>
+  <h4>Objectifs :</h4>
+  <ul>
+    <li>Acquérir les compétences nécessaires à la programmation d’applications dans le langage Orienté Objet JAVA.</li>
+  </ul>
+  <h4>Programme :</h4>
+  <ul>
+    <li>Création de Classes / Objets</li>
+    <li>Encapsulation, Composition, Héritage, Interface, Polymorphisme, Exception, AWT</li>
+    <li>Création d’Applets</li>
+    <li>Création d’applications Client/Serveur</li>
+  </ul>
+
+  <h2>UE 3 – DEVELOPPEMENT DE SERVICES WEB (146 H)</h2>
+  <h3>Programmation HTML/CSS</h3>
+  <h4>Objectifs :</h4>
+  <ul>
+    <li>Acquérir les Bases nécessaires d’un langage à Balise pour la création de page WEB</li>
+  </ul>
+  <h4>Programme :</h4>
+  <ul>
+    <li>Apprentissage de langage</li>
+    <li>Mise en oeuvre de formulaire.</li>
+    <li>Mise en oeuvre de page de style CSS.</li>
+  </ul>
+  <h3>Programmation JavaScript</h3>
+  <h4>Objectifs :</h4>
+  <ul>
+    <li>Acquérir les techniques de programmation coté client en JavaScript</li>
+  </ul>
+  <h4>Programme :</h4>
+  <ul>
+    <li>Apprentissage du langage.</li>
+    <li>Approche du DOM et du HTML.</li>
+  </ul>
+  <h3>Conception sites web : CMS</h3>
+  <h4>Objectifs :</h4>
+  <ul>
+    <li>Acquérir les techniques permettant de concevoir des sites web utilisant des CMS</li>
+  </ul>
+  <h4>Programme :</h4>
+  <ul>
+    <li>Sites web</li>
+    <li>CMS Joomla</li>
+    <li>CMS WordPress</li>
+  </ul>
+  <h3>Programmation PHP</h3>
+  <h4>Objectifs :</h4>
+  <ul>
+    <li>Acquérir les techniques de programmation côté serveur en PHP</li>
+  </ul>
+  <h4>Programme :</h4>
+  <ul>
+    <li>Apprentissage du langage.</li>
+    <li>Mise en application sur serveur APACHE.</li>
+    <li>Interfaçage avec une Base de Données MySQL.</li>
+  </ul>
+  <h3>Programmation Dynamique XML</h3>
+  <h4>Objectifs :</h4>
+  <ul>
+    <li>Acquérir les Bases nécessaires d’un langage à Balise XML</li>
+  </ul>
+  <h4>Programme :</h4>
+  <ul>
+    <li>Apprentissage de langage XML / DTD / XLM Schéma</li>
+    <li>XPATH / XSL-T</li>
+    <li>SAX et DOM</li>
+    <li>Approche du Web Services SAOP (J2EE)</li>
+  </ul>
+</div>
+
+<?php include('includes/footer.php') ?>
