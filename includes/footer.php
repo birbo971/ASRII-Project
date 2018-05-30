f9d38293a2dcd3eb0a6cab7c9a0da33ed3c5e9e0
CorentinPuaud <37401262+CorentinPuaud@users.noreply.github.com>
2018-05-30 10:37:19 +0200

    Debug

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,8 @@
   </body>
-  <footer>
+  <footer class="footer">
+    <nav class="navbar fixed-bottom navbar-expand-sm navbar-dark bg-dark navfooter">
+      <p id="footer-text">Copyright 2018 by Corentin | Brice | Olivier</p>
+    </nav>
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/script.js"></script>
--- objectifrythmes.php
+++ objectifrythmes.php
@@ -7,18 +7,25 @@ include('includes/header.php'); ?>
   <p class="display-4">Licence Pro Métiers de l'Informatique : Application Web</p>
     <hr>
   <br/>
-  <p class="h3" id="Objectif">Objectifs de la licence</p>
+  <p class="h3" id="Objectif">Objectifs de la formation</p>
   <hr class="hrmoyen">
-    <p class="lead text-center">Le diplôme a pour objet de former aux comportements et techniques de conception et d’administration destinés à l’analyse, la conception,
-    la mise à disposition et au maintien en condition opérationnelle des systèmes d’information, par le biais de 2 parcours :</p><br/>
-    <p class="lead">Parcours CIGSINFO ABDD : Conception, Intégration et Gestion des Systèmes d’Information - Analyse de Bases de Données et systèmes décisionnels</p>
-    <p class="lead">Parcours CIGSINFO APSRA : Conception, Intégration et Gestion des Systèmes d’Information - Analyse de Production Systèmes Réseaux et Application</p>
+    <p class="lead text-center">Former des jeunes de niveau BAC+2 aux métiers de l’Internet et de responsable informatique chargé de la gestion d’un réseau local et du développement et déploiement de produits et de services Intranet ou / et Internet.
+      Les postes visés sont intermédiaires entre des techniciens supérieurs et des ingénieurs,principalement, orientés vers les PME/PMI.</p>
+    <br/>
+    <p class="lead text-center"> Parcours ASR2I : Administration et Sécurisation des Réseaux et services Internet et Intranet.</p>
+    <ol><p class="text-center"><li>Configurer un réseau local pour un ensemble de postes informatiques, dans un environnement hétérogène
+      (Windows, Linux) et assurer l’interconnexion avec le monde extérieur.</li></p>
+    <p class="text-center"><li>Administrer au quotidien les serveurs rattachés au réseau informatique pour garantir, en permanence, leur
+      disponibilité et leur sécurité.</li></p>
+    <p class="lead text-center"> Parcours DAW2I : Développement avancé d’Applications web Internet et Intranet.</p>
+      <p class="text-center"><li>Développer des applications WEB après avoir défini avec le client les besoins et le cahier des charges.</li></p>
+      <p class="text-center"><li>Intégrer des services et des applications Web aussi bien pour un Intranet que pour l’Internet.</li></p></ol>
     <br/>
   <p class="h3" id="Rythme">Rythme de la licence</p>
   <hr class="hrmoyen">
-    <p class="lead">Octobre, novembre et décembre : une semaine sur deux en entreprise</p>
-<p class="lead">De janvier à mai : deux semaines successives par mois</p>
-<p class="lead">A partir du mois de juin : 100% en entreprise.</p>
+    <p class="lead">Octobre, novembre et décembre : une semaine sur deux en entreprise</li></p>
+<p class="lead"><li>De janvier à mai : deux semaines successives par mois</p>
+<p class="lead"><li>A partir du mois de juin : 100% en entreprise.</li></p>
 </div>
 
 <?php include('includes/footer.php') ?>
