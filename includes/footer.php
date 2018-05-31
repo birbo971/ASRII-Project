457badc4d74985870ad56ab9da3b04738aafabf0
CorentinPuaud <37401262+CorentinPuaud@users.noreply.github.com>
2018-05-31 12:41:47 +0200

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,19 +1,17 @@
   </body>
   <footer class="footer">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark navfooter">
-      <div class="col-2">
+      <div class="col-10">
         <img src="img/logo.png" alt="logo" class="logo">
-      </div>
-      <div class="col-8">
         <p id="footer-text">Copyright 2018 by Corentin | Brice | Olivier</p>
       </div>
       <div class="col-2">
-        <a class="btn btn-social-icon btn-facebook">
-          <span class="fab fa-facebook-square"></span>
-        </a>
-        <a class="btn btn-social-icon btn-pinterest">
-          <span class="fab fa-youtube"></span>
-        </a>
+        <!-- <a class="btn btn-social-icon btn-facebook"> -->
+          <a class="fab fa-facebook-square" href="https://www.facebook.com/cfaeve/"></a>
+        <!-- </a> -->
+        <!-- <a class="btn btn-social-icon btn-pinterest"> -->
+          <a class="fab fa-youtube" href="https://www.youtube.com/channel/UCz7vahAm8eokrcCQNGCJDug"></a>
+        <!-- </a> -->
       </div>
     </nav>
     <script src="js/jquery.min.js"></script>
