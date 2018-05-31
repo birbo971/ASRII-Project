d5dbf8955edaa836a051eaf759bb20f27519058e
CorentinPuaud <37401262+CorentinPuaud@users.noreply.github.com>
2018-05-31 10:34:44 +0200

    Modif footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,20 @@
   </body>
   <footer class="footer">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark navfooter">
-      <p id="footer-text">Copyright 2018 by Corentin | Brice | Olivier</p>
+      <div class="col-2">
+        <img src="img/logo.png" alt="logo" class="logo">
+      </div>
+      <div class="col-8">
+        <p id="footer-text">Copyright 2018 by Corentin | Brice | Olivier</p>
+      </div>
+      <div class="col-2">
+        <a class="btn btn-social-icon btn-facebook">
+          <span class="fab fa-facebook-square"></span>
+        </a>
+        <a class="btn btn-social-icon btn-pinterest">
+          <span class="fab fa-youtube"></span>
+        </a>
+      </div>
     </nav>
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
