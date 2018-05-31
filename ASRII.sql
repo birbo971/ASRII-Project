c97c3d25421878930bf1e8853fb17eccdd05c134
Brice VIGNAL <vignal.brice@gmail.com>
2018-05-31 16:54:55 +0200

    base

--- ASRII.sql
+++ ASRII.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mer 30 Mai 2018 à 08:08
+-- Généré le :  Jeu 31 Mai 2018 à 14:54
 -- Version du serveur :  5.6.20-log
 -- Version de PHP :  5.4.31
 
@@ -27,19 +27,26 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `ajout_projet` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `nom` varchar(255) DEFAULT NULL,
   `email` text,
   `titre` varchar(255) DEFAULT NULL,
   `description` text,
   `etat` enum('en attente','refuse','valide','') NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `emploi_du_temps`
+--
+
+CREATE TABLE IF NOT EXISTS `emploi_du_temps` (
+  `id_user` int(11) NOT NULL,
+`id_horaires` int(11) NOT NULL,
+  `horaires` varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-
--- Index pour la table `ajout_projet`
---
-ALTER TABLE `ajout_projet`
- ADD PRIMARY KEY (`id`);
 -- --------------------------------------------------------
 
 --
@@ -72,16 +79,14 @@ CREATE TABLE IF NOT EXISTS `entreprise` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `etudiant`
+-- Structure de la table `notes`
 --
 
-CREATE TABLE IF NOT EXISTS `etudiant` (
-`id_etd` int(11) NOT NULL,
-  `nom` varchar(255) NOT NULL,
-  `prenom` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `date_naissance` date NOT NULL,
-  `mdp` int(50) NOT NULL
+CREATE TABLE IF NOT EXISTS `notes` (
+  `id_user` int(11) NOT NULL,
+  `notes` varchar(255) NOT NULL,
+  `matieres` int(11) NOT NULL,
+`id_notes` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -97,12 +102,46 @@ CREATE TABLE IF NOT EXISTS `personnel miaw` (
   `email` varchar(255) NOT NULL,
   `date_naissance` date NOT NULL,
   `mdp` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+`id_users` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mdp` int(50) NOT NULL,
+  `etat` enum('enseignant','entreprise','etudiant','personnel miaw') NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id_users`, `nom`, `prenom`, `email`, `mdp`, `etat`) VALUES
+(1, 'Vignal', 'Brice', 'vignal.brice@gmail.com', 200597, 'etudiant'),
+(2, 'Khalifa', 'Djemal', 'khalifa.djemal@gmail.com', 123456, 'enseignant');
 
 --
 -- Index pour les tables exportées
 --
 
+--
+-- Index pour la table `ajout_projet`
+--
+ALTER TABLE `ajout_projet`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `emploi_du_temps`
+--
+ALTER TABLE `emploi_du_temps`
+ ADD PRIMARY KEY (`id_horaires`);
 
 --
 -- Index pour la table `enseignant`
@@ -117,10 +156,10 @@ ALTER TABLE `entreprise`
  ADD PRIMARY KEY (`id_entr`);
 
 --
--- Index pour la table `etudiant`
+-- Index pour la table `notes`
 --
-ALTER TABLE `etudiant`
- ADD PRIMARY KEY (`id_etd`);
+ALTER TABLE `notes`
+ ADD PRIMARY KEY (`id_notes`);
 
 --
 -- Index pour la table `personnel miaw`
@@ -129,14 +168,20 @@ ALTER TABLE `personnel miaw`
  ADD PRIMARY KEY (`id_pers`);
 
 --
+-- Index pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+ ADD PRIMARY KEY (`id_users`);
+
+--
 -- AUTO_INCREMENT pour les tables exportées
 --
 
 --
--- AUTO_INCREMENT pour la table `ajout_projet`
+-- AUTO_INCREMENT pour la table `emploi_du_temps`
 --
-ALTER TABLE `ajout_projet`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `emploi_du_temps`
+MODIFY `id_horaires` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `enseignant`
 --
@@ -148,15 +193,20 @@ MODIFY `id_ens` int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `entreprise`
 MODIFY `id_entr` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT pour la table `etudiant`
+-- AUTO_INCREMENT pour la table `notes`
 --
-ALTER TABLE `etudiant`
-MODIFY `id_etd` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `notes`
+MODIFY `id_notes` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `personnel miaw`
 --
 ALTER TABLE `personnel miaw`
 MODIFY `id_pers` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+MODIFY `id_users` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
