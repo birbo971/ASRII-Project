6e71a0cc0766c7c95f9bfbf1c12737dea06cda84
CorentinPuaud <37401262+CorentinPuaud@users.noreply.github.com>
2018-06-01 09:38:06 +0200

    aa

---
+++ emploidutemps.php
@@ -0,0 +1,14 @@
+<?php
+$etudiant ='active';
+$titre ="Emploi du temps";
+include('includes/header.php'); ?>
+<?php include('includes/navbar.php'); ?>
+<?php
+ifIsConnected();
+?>
+<div class="container">
+  <h1>Emploi du temps</h1>
+    <hr>
+    <br/>
+</div>
+<?php include("includes/footer.php"); ?>
--- espaceetudiant.php
+++ espaceetudiant.php
@@ -11,7 +11,7 @@ ifIsConnected();
     <hr>
     <br/>
   <li><a href="notes.php">Acceder à ses notes.</a>&nbsp;<i class="fas fa-edit"></i></li><br/>
-  <li><a href="">Acceder à l'emploi du temps.</a>&nbsp;<i class="fas fa-newspaper"></i></li><br/>
+  <li><a href="emploidutemps.php">Acceder à l'emploi du temps.</a>&nbsp;<i class="fas fa-newspaper"></i></li><br/>
   <li><a href="">Consulter les projets tuteurés disponibles.</a>&nbsp;<i class="fas fa-clipboard"></i></li><br/>
   <li><a href="">Consulter les offres d'alternances ouvertes à candidature.</a>&nbsp;<i class="fas fa-handshake"></i></li><br/>
   <li><a href="">Consulter les supports de cours.</a>&nbsp;<i class="fab fa-leanpub"></i></li>
