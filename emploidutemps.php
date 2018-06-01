b92c122a79d4827e86830ff737f5b4d52829c342
CorentinPuaud <37401262+CorentinPuaud@users.noreply.github.com>
2018-06-01 10:43:25 +0200

    edt

--- emploidutemps.php
+++ emploidutemps.php
@@ -8,7 +8,92 @@ ifIsConnected();
 ?>
 <div class="container">
   <h1>Emploi du temps</h1>
-    <hr>
-    <br/>
+  <hr>
+  <table>
+    <thead>
+      <tr>
+        <th>
+        </th>
+        <th>
+          Lundi *
+        </th>
+        <th>
+          Mardi *
+        </th>
+        <th>
+          Mercredi *
+        </th>
+        <th>
+          Jeudi *
+        </th>
+        <th>
+          Vendredi *
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>
+          <small>09h00</small>
+          <br>
+          <small>13h00</small>
+        </td>
+        <td>
+          *
+        </td>
+        <td>
+          *
+        </td>
+        <td>
+          *
+        </td>
+        <td>
+          *
+        </td>
+        <td>
+          *
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <small>13h00</small>
+          <br>
+          <small>14h00</small>
+        </td>
+        <td>
+        </td>
+        <td>
+        </td>
+        <td>
+        </td>
+        <td>
+        </td>
+        <td>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <small>14h00</small>
+          <br>
+          <small>18h00</small>
+        </td>
+        <td>
+          *
+        </td>
+        <td>
+          *
+        </td>
+        <td>
+          *
+        </td>
+        <td>
+          *
+        </td>
+        <td>
+          *
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </div>
 <?php include("includes/footer.php"); ?>
