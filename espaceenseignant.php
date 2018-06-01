1cc10ecd65aa4ea4a51926bd28d488844ad4c1d4
CorentinPuaud <37401262+CorentinPuaud@users.noreply.github.com>
2018-06-01 10:27:03 +0200

    Modif espace enseigant

--- espaceenseignant.php
+++ espaceenseignant.php
@@ -8,10 +8,10 @@ $titre ="Espace Enseignant";
 ifIsConnected();
 ?>
 <!--Content-->
-<div class="container">
+<div class="container center">
   <h1>Espace Enseignant</h1>
   <hr>
-  <h2 class="center">Notes <i class="fas fa-edit"></i></h2>
+  <h2>Notes <i class="fas fa-edit"></i></h2>
   <hr class="hrmoyen"/>
   <ul>
     <li>
@@ -21,14 +21,14 @@ ifIsConnected();
       <a href="">Consulter ses notes déposées.</a>
     </li>
   </ul>
-  <h2 class="center">Emploi du temp personnel <i class="fas fa-newspaper"></i></h2>
+  <h2>Emploi du temp personnel <i class="fas fa-newspaper"></i></h2>
   <hr class="hrmoyen"/>
   <ul>
     <li>
       <a href="">Acceder à l'emploi du temps personnel.</a>
     </li>
   </ul>
-  <h2 class="center">Support de cours <i class="fas fa-copy"></i></h2>
+  <h2>Support de cours <i class="fas fa-copy"></i></h2>
   <hr class="hrmoyen"/>
   <ul>
     <li>
