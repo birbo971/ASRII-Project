61c26551e971154eac4ecb12304df860e2376ffd
CorentinPuaud <37401262+CorentinPuaud@users.noreply.github.com>
2018-06-12 09:17:06 +0200

    root

--- includes/DB.php
+++ includes/DB.php
@@ -8,7 +8,7 @@ class DB
        {
            try
            {
-               self::$instance = new PDO('mysql:host=localhost;dbname=asrii', 'root', '');
+               self::$instance = new PDO('mysql:host=localhost;dbname=asrii', 'root', 'root');
            }
            catch(PDOException $e)
            {
