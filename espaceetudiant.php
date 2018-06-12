de3bdfd50d825c1b0f61fb7697dc59aef6301d98
CorentinPuaud <37401262+CorentinPuaud@users.noreply.github.com>
2018-06-12 09:31:24 +0200

    MaJ

--- espaceetudiant.php
+++ espaceetudiant.php
@@ -6,14 +6,43 @@ include('includes/header.php'); ?>
 <?php
 ifIsConnected();
 ?>
-<div class="container">
+<div class="container center">
   <h1>Espace Etudiant</h1>
-    <hr>
-    <br/>
-  <li><a href="notes.php">Acceder à ses notes.</a>&nbsp;<i class="fas fa-edit"></i></li><br/>
-  <li><a href="emploidutemps.php">Acceder à l'emploi du temps.</a>&nbsp;<i class="fas fa-newspaper"></i></li><br/>
-  <li><a href="">Consulter les projets tuteurés disponibles.</a>&nbsp;<i class="fas fa-clipboard"></i></li><br/>
-  <li><a href="">Consulter les offres d'alternances ouvertes à candidature.</a>&nbsp;<i class="fas fa-handshake"></i></li><br/>
-  <li><a href="">Consulter les supports de cours.</a>&nbsp;<i class="fab fa-leanpub"></i></li>
+  <hr>
+  <h2>Notes <i class="fas fa-edit"></i></h2>
+  <hr class="hrmoyen"/>
+  <ul>
+    <li>
+      <a href="notes.php">Acceder à ses notes.</a>
+    </li>
+  </ul>
+  <h2>Emploi du temps <i class="fas fa-newspaper"></i></h2>
+  <hr class="hrmoyen"/>
+  <ul>
+    <li>
+      <a href="emploidutemps.php">Acceder à l'emploi du temps.</a>
+    </li>
+  </ul>
+  <h2>Projets tuteurés <i class="fas fa-clipboard"></i></h2>
+  <hr class="hrmoyen"/>
+  <ul>
+    <li>
+      <a href="">Consulter les projets tuteurés disponibles.</a>
+    </li>
+  </ul>
+  <h2>ALternance <i class="fas fa-handshake"></i></h2>
+  <hr class="hrmoyen"/>
+  <ul>
+    <li>
+      <a href="">Consulter les offres d'alternances ouvertes à candidature.</a>
+    </li>
+  </ul>
+  <h2>Support de cours <i class="fab fa-leanpub"></i></h2>
+  <hr class="hrmoyen"/>
+  <ul>
+    <li>
+      <a href="">Consulter les supports de cours.</a>
+    </li>
+  </ul>
 </div>
 <?php include("includes/footer.php"); ?>
