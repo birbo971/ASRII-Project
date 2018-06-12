cfe2fbc28f561290515b9eb040b2300323f51870
CorentinPuaud <37401262+CorentinPuaud@users.noreply.github.com>
2018-06-12 12:42:06 +0200

    Jolie

--- consulteroffresalternances.php
+++ consulteroffresalternances.php
@@ -8,23 +8,28 @@ $titre="Consulter une offres d'alternance";
   <h1>Les offres d'alternances</h1>
 
     <hr>
-    <br/>
+    <div class="row">
 
     <?php
-	echo'<table class="table table-bordered">';
-      echo'<thead class="thead-dark">';
-      echo'<tr><th>Titre</th>';
-      echo'<th>Description</th>';
-      echo'<th>Experience Requise</th>';
-      echo'</thead><tbody>';
+	// echo'<table class="table table-bordered">';
+  //     echo'<thead class="thead-dark">';
+  //     echo'<tr><th>Titre</th>';
+  //     echo'<th>Description</th>';
+  //     echo'<th>Experience Requise</th>';
+  //     echo'</thead><tbody>';
     while ($res = $consultation->fetch()){
-
-      echo'<tr><td>'.$res['titre'].'</td><td>'.$res['description'].'</td><td>'.$res['experience'].' Mois </td></tr>';
-
+      // echo'<tr><td>'.$res['titre'].'</td><td>'.$res['description'].'</td><td>'.$res['experience'].' Mois </td></tr>';
+      echo '<div class="col-md-6 col-sm-12 center">
+              <h2>' . $res['titre'] . '</h2>
+              <hr class="hrmoyen"/>
+              <p>' . $res['description'] . '</p>
+              <p class="lead">Experience requise : ' . $res['experience'] . ' Mois</p>
+              <p class="lead">Contact : <a href="mailto:' . $res['email'] . '">' . $res['email'] . '</a></p>
+            </div>';
     }
-	echo'</tbody></table>';
+	// echo'</tbody></table>';
 	?>
-
+</div>
 </div>
 <?php
 include('includes/footer.php');
